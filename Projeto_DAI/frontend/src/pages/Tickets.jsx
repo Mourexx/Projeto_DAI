6fdb22fe301359cd81ee7ceda6ad974a4adfc962
João Costa <jogis6993@gmail.com>
2026-03-07 14:49:05 +0000

    fix: mover projeto para raiz do repositório

--- Projeto_DAI/frontend/src/pages/Tickets.jsx
+++
@@ -1,8 +0,0 @@
-export default function Tickets() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Tickets</h1>
-      <p className="text-gray-500 mt-2">Página em construção...</p>
-    </div>
-  )
-}
